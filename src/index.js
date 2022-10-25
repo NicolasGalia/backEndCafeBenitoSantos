ebf897c6364ad244dc5c975f175c889447bdc8e6
Nicolas GaliaM <nicolasgaliam@gmail.com>
2022-10-25 17:36:43 -0300

    terminamo la clase

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import path from 'path';
+
 
 //crear una instancia de express 
 const app = express();
@@ -10,5 +14,17 @@ app.listen( app.get('port'), ()=>{
 })
 
 //middlewears: son funciones que se ejecutan antes de llegar a las rutas 
+app.use(cors()); //cors : permite peticiones remotas
+//permite recibir y usar objjetos en formato json 
+app.use(express.json()); 
+app.use(express.urlencoded({extended: true}));
+//informacion extra 
+app.use(morgan('dev'));
+// cargar un archivo estatico
+app.use(express.static(path.join(__dirname, '../public')))
 
 //rutas 
+app.get('/', (req, res)=>{
+res.send('hola desde el backEnd en la peticion get')
+
+})
