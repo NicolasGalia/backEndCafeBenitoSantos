56449368e93b218e4b0e5b479e9a0ef093ec7ba6
Nicolas GaliaM <nicolasgaliam@gmail.com>
2022-10-26 08:59:44 -0300

    terminamos de crear los productos en la base de datos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,10 +1,25 @@
-export const listarProductos = (req, res)=>{
-        res.send('hola desde el backEnd en la peticion get')
+import Producto from "../models/productos";
+
+export const listarProductos = (req, res) => {
+  res.send("hola desde el backEnd en la peticion get");
 };
 
-export const crearProducto = (req, res)=>{
-    console.log(req.body);
-    //validar los datos del objeto 
-    //guardar el objeto en la base de datos 
-    res.send('aqui tendria que crear un producto')
+export const crearProducto = async (req, res) => {
+  try {
+    // console.log(req.body);
+    //validar los datos del objeto
+
+    const productoNuevo = new Producto(req.body);
+    //guarda ese objeto en la base de datos
+    await productoNuevo.save();
+
+    res.status(201).json({
+      mensaje: "el producto fue creado correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(404)
+      .json({ mensaje: "error al intentar agregar un nuevo producto" });
+  }
 };
