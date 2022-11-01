399b5c6370e2afa633ee63c1e854e7cf4bde155c
Nicolas GaliaM <nicolasgaliam@gmail.com>
2022-11-01 09:07:51 -0300

    terminamos de realizar la conexion a la base de datos

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-const url = 'mongodb://localhost:27017/cafe-benito-santos'
+// const url = 'mongodb://localhost:27017/cafe-benito-santos' local 
+const url = 'mongodb+srv://nicolasgaliam:nico12345@cluster0.7hzvgkb.mongodb.net/cafebenitosantos'
 
 mongoose.connect(url);
 
